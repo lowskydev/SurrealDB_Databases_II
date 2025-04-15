097c0f58f65658996a6a8091e1eb6582bc4611ee
lowskydev <code@lowsky.dev>
2025-04-15 11:50:14 +0200

    tasks written to a file

--- tasks.py
+++ tasks.py
@@ -1,7 +1,9 @@
 from surrealdb import Surreal
 import time
+from contextlib import redirect_stdout
+from datetime import date, datetime
 
-SOURCE_NODE_NAME = "/c/en/vape"
+SOURCE_NODE_NAME = "/c/en/linux"
 
 
 def print_node_info(input_node, show_full=False, indent=2):
@@ -85,223 +87,220 @@ with Surreal("ws://localhost:8000/rpc") as db:
     if not source_node:
         raise Exception(f"ERROR: Node '{SOURCE_NODE_NAME}' not found in SurrealDB")
 
-    source_node_id = source_node['id']
-    source_node_label = source_node['label']
-    print(f"--- SOURCE NODE ---")
+    with open(f"output_{datetime.now().strftime('%Y-%m-%d--%H-%M')}.txt", "w", 1) as file:
+        with redirect_stdout(file):
 
-    print_node_info(source_node, show_full=True)
+            source_node_id = source_node['id']
+            source_node_label = source_node['label']
+            print(f"--- SOURCE NODE ---")
 
-    ##############################
-    ### Task 1: Find all successors of a given node.
-    print(f"--- TASK 1 ---")
-    time_start = time.time()
+            print_node_info(source_node, show_full=True)
 
-    successors = find_successors(source_node_id)
+            ##############################
+            ### Task 1: Find all successors of a given node.
+            print(f"--- TASK 1 ---")
+            time_start = time.time()
 
-    print(f"Successors of {SOURCE_NODE_NAME}:")
-    for successor in successors:
-        print_node_info(successor)
+            successors = find_successors(source_node_id)
 
-    time_end = time.time()
-    print_time(time_start, time_end)
-    ###############################
-    ### Task 2: Count all successors of a given node.
-    print(f"--- TASK 2 ---")
-    time_start = time.time()
+            print(f"Successors of {SOURCE_NODE_NAME}:")
+            for successor in successors:
+                print_node_info(successor)
 
-    successor_count = len(successors)
+            time_end = time.time()
+            print_time(time_start, time_end)
+            ###############################
+            ### Task 2: Count all successors of a given node.
+            print(f"--- TASK 2 ---")
+            time_start = time.time()
 
-    print(f"Total successors of {SOURCE_NODE_NAME}: {successor_count}")
+            successor_count = len(successors)
 
-    time_end = time.time()
-    print_time(time_start, time_end)
-    ###############################
-    ### Task 3: Find all predecessors of a given node.
-    print(f"--- TASK 3 ---")
-    time_start = time.time()
+            print(f"Total successors of {SOURCE_NODE_NAME}: {successor_count}")
 
-    predecessors = find_predecessors(source_node_id)
+            time_end = time.time()
+            print_time(time_start, time_end)
+            ###############################
+            ### Task 3: Find all predecessors of a given node.
+            print(f"--- TASK 3 ---")
+            time_start = time.time()
 
-    print(f"Predecessors of {SOURCE_NODE_NAME}:")
-    for predecessor in predecessors:
-        print_node_info(predecessor)
+            predecessors = find_predecessors(source_node_id)
 
-    time_end = time.time()
-    print_time(time_start, time_end)
-    ###############################
-    ### Task 4: Count all predecessors of a given node.
-    print(f"--- TASK 4 ---")
-    time_start = time.time()
+            print(f"Predecessors of {SOURCE_NODE_NAME}:")
+            for predecessor in predecessors:
+                print_node_info(predecessor)
 
-    predecessor_count = len(predecessors)
+            time_end = time.time()
+            print_time(time_start, time_end)
+            ###############################
+            ### Task 4: Count all predecessors of a given node.
+            print(f"--- TASK 4 ---")
+            time_start = time.time()
 
-    print(f"Total predecessors of {SOURCE_NODE_NAME}: {predecessor_count}")
+            predecessor_count = len(predecessors)
 
-    time_end = time.time()
-    print_time(time_start, time_end)
-    ################################
-    ### Task 5: Find all neighbors of a given node.
-    print(f"--- TASK 5 ---")
-    time_start = time.time()
+            print(f"Total predecessors of {SOURCE_NODE_NAME}: {predecessor_count}")
 
-    neighbours = find_neighbours(source_node_id)
+            time_end = time.time()
+            print_time(time_start, time_end)
+            ################################
+            ### Task 5: Find all neighbors of a given node.
+            print(f"--- TASK 5 ---")
+            time_start = time.time()
 
-    print(f"Neighbours of {SOURCE_NODE_NAME}:")
-    for neighbour in neighbours:
-        print_node_info(neighbour)
+            neighbours = find_neighbours(source_node_id)
 
-    time_end = time.time()
-    print_time(time_start, time_end)
-    #################################
-    ### Task 6: Count all neighbours of a given node.
-    print(f"--- TASK 6 ---")
-    time_start = time.time()
+            print(f"Neighbours of {SOURCE_NODE_NAME}:")
+            for neighbour in neighbours:
+                print_node_info(neighbour)
 
-    neighbour_count = len(neighbours)
+            time_end = time.time()
+            print_time(time_start, time_end)
+            #################################
+            ### Task 6: Count all neighbours of a given node.
+            print(f"--- TASK 6 ---")
+            time_start = time.time()
 
-    print(f"Total neighbours of {SOURCE_NODE_NAME}: {neighbour_count}")
+            neighbour_count = len(neighbours)
 
-    time_end = time.time()
-    print_time(time_start, time_end)
-    ##################################
-    ### Task 7: Find all grandchildren (successors of successors) of a given node.
-    print(f"--- TASK 7 ---")
-    time_start = time.time()
+            print(f"Total neighbours of {SOURCE_NODE_NAME}: {neighbour_count}")
 
-    for successor in successors:
-        grandchildren = find_successors(successor['id'])
-        if grandchildren:
-            print(f"  Grandchildren of {successor['node']}:")
-            for grandchild in grandchildren:
-                print_node_info(grandchild, indent=4)
+            time_end = time.time()
+            print_time(time_start, time_end)
+            ##################################
+            ### Task 7: Find all grandchildren (successors of successors) of a given node.
+            print(f"--- TASK 7 ---")
+            time_start = time.time()
 
-    time_end = time.time()
-    print_time(time_start, time_end)
-    ####################################
-    ### Task 8: Find all grandparents (predecessors of predecessors) of a given node.
-    print(f"--- TASK 8 ---")
-    time_start = time.time()
+            for successor in successors:
+                grandchildren = find_successors(successor['id'])
+                if grandchildren:
+                    print(f"  Grandchildren of {successor['node']}:")
+                    for grandchild in grandchildren:
+                        print_node_info(grandchild, indent=4)
 
-    for predecessor in predecessors:
-        grandparents = find_predecessors(predecessor['id'])
-        if grandparents:
-            print(f"  Grandparents of {predecessor['node']}:")
+            time_end = time.time()
+            print_time(time_start, time_end)
+            ####################################
+            ### Task 8: Find all grandparents (predecessors of predecessors) of a given node.
+            print(f"--- TASK 8 ---")
+            time_start = time.time()
 
-            for grandparent in grandparents:
-                print_node_info(grandparent, indent=4)
+            for predecessor in predecessors:
+                grandparents = find_predecessors(predecessor['id'])
+                if grandparents:
+                    print(f"  Grandparents of {predecessor['node']}:")
 
-    time_end = time.time()
-    print_time(time_start, time_end)
-    ####################################
-    ### Task 9: Count how many nodes there are.
-    print(f"--- TASK 9 ---")
-    time_start = time.time()
+                    for grandparent in grandparents:
+                        print_node_info(grandparent, indent=4)
 
-    node_count = db.query("RETURN count(SELECT * FROM Nodes PARALLEL);")
-    # BARTOSZ - IS THERE FASTER WAY TO DO IT?
-    # There's 'INFO FOR TABLE Nodes' query but it doesn't return the count of nodes in the table
-    # maybe we could add some metadate there perhaps?
+            time_end = time.time()
+            print_time(time_start, time_end)
+            ####################################
+            ### Task 9: Count how many nodes there are.
+            print(f"--- TASK 9 ---")
+            time_start = time.time()
 
-    print(f"Total nodes in the database: {node_count}")
+            node_count = db.query("RETURN count(SELECT * FROM Nodes PARALLEL);")
+            # BARTOSZ - IS THERE FASTER WAY TO DO IT?
+            # There's 'INFO FOR TABLE Nodes' query but it doesn't return the count of nodes in the table
+            # maybe we could add some metadate there perhaps?
 
-    time_end = time.time()
-    print_time(time_start, time_end)
+            print(f"Total nodes in the database: {node_count}")
 
-    ####################################
-    ### Task 10: Count nodes which do not have any successors.
-    print(f"--- TASK 10 ---")
+            time_end = time.time()
+            print_time(time_start, time_end)
 
-    # test 1
-    # time: 186 minutes
-    # result: 649,184
-    time_start = time.time()
+            ####################################
+            ### Task 10: Count nodes which do not have any successors.
+            print(f"--- TASK 10 ---")
 
-    no_successor_count = db.query("RETURN count(SELECT * FROM Nodes WHERE count(->edge->Nodes) = 0 PARALLEL);")
+            # test 1
+            # time: 186 minutes
+            # result: 649,184
+            time_start = time.time()
 
-    print(f"Total nodes without successors: {no_successor_count}")
+            no_successor_count = db.query("RETURN count(SELECT * FROM Nodes WHERE count(->edge->Nodes) = 0 PARALLEL);")
 
-    time_end = time.time()
-    print_time(time_start, time_end)
+            print(f"Total nodes without successors: {no_successor_count}")
 
-    """
-    # test 2
-    # time: 403 minutes
-    # result: 649,184
-    time_start = time.time()
+            time_end = time.time()
+            print_time(time_start, time_end)
 
-    all_nodes = db.query("SELECT * FROM Nodes PARALLEL;")
+            """
+            # test 2
+            # time: 403 minutes
+            # result: 649,184
+            time_start = time.time()
 
-    no_successor_count = 0
-    for i, node in enumerate(all_nodes):
-        if not find_successors(node['id']):
-            no_successor_count += 1
+            all_nodes = db.query("SELECT * FROM Nodes PARALLEL;")
 
-        # if i % 10000 == 0 and i != 0:
-            # print(f"Progress: {(i / len(all_nodes)) * 100:.2f}%")
+            no_successor_count = 0
+            for i, node in enumerate(all_nodes):
+                if not find_successors(node['id']):
+                    no_successor_count += 1
 
-    print(f"Total nodes without successors (2): {no_successor_count}")
+                # if i % 10000 == 0 and i != 0:
+                    # print(f"Progress: {(i / len(all_nodes)) * 100:.2f}%")
 
-    time_end = time.time()
-    print_time(time_start, time_end)
+            print(f"Total nodes without successors (2): {no_successor_count}")
 
-    # what if we insert a count predecessors and successors fields into Nodes?
-    """
-    ########################################
-    ### Task 11: Count nodes which do not have any predecessors.
-    print(f"--- TASK 11 ---")
-    time_start = time.time()
+            time_end = time.time()
+            print_time(time_start, time_end)
 
-    no_predecessor_count = db.query("RETURN count(SELECT * FROM Nodes WHERE count(<-edge<-Nodes) = 0 PARALLEL);")
+            # what if we insert a count predecessors and successors fields into Nodes?
+            """
+            ########################################
+            ### Task 11: Count nodes which do not have any predecessors.
+            print(f"--- TASK 11 ---")
+            time_start = time.time()
 
-    print(f"Total nodes without predecessors: {no_predecessor_count}")
+            no_predecessor_count = db.query("RETURN count(SELECT * FROM Nodes WHERE count(<-edge<-Nodes) = 0 PARALLEL);")
 
-    time_end = time.time()
-    print_time(time_start, time_end)
+            print(f"Total nodes without predecessors: {no_predecessor_count}")
 
-    ########################################
-    ### Task 12: Find nodes with the most neighbours.
-    print(f"--- TASK 12 ---")
-    time_start = time.time()
+            time_end = time.time()
+            print_time(time_start, time_end)
 
-    max_neighbours = 0
-    max_neighbours_nodes = list()
-    all_nodes = db.query("SELECT * FROM Nodes PARALLEL;")
-    for i, node in enumerate(all_nodes):
-        neighbours = find_neighbours(node['id'])
-        if len(neighbours) > max_neighbours:
-            max_neighbours = len(neighbours)
+            ########################################
+            ### Task 12: Find nodes with the most neighbours.
+            print(f"--- TASK 12 ---")
+            time_start = time.time()
+
+            max_neighbours = 0
             max_neighbours_nodes = list()
-            max_neighbours_nodes.append(node['node'])
-        elif len(neighbours) == max_neighbours:
-            max_neighbours_nodes.append(node['node'])
+            all_nodes = db.query("SELECT * FROM Nodes PARALLEL;")
+            for i, node in enumerate(all_nodes):
+                neighbours = find_neighbours(node['id'])
+                if len(neighbours) > max_neighbours:
+                    max_neighbours = len(neighbours)
+                    max_neighbours_nodes = list()
+                    max_neighbours_nodes.append(node['node'])
+                elif len(neighbours) == max_neighbours:
+                    max_neighbours_nodes.append(node['node'])
 
-        if i % 10000 == 0 and i != 0:
-            print(f"Progress: {(i / len(all_nodes)) * 100:.2f}%")
+            print(f"Nodes with the most neighbours ({max_neighbours}):")
+            for node in max_neighbours_nodes:
+                print_node_info(node)
 
-    print(f"Nodes with the most neighbours ({max_neighbours}):")
-    for node in max_neighbours_nodes:
-        print_node_info(node)
+            time_end = time.time()
+            print_time(time_start, time_end)
 
-    time_end = time.time()
-    print_time(time_start, time_end)
+            ########################################
+            ### Task 13: Count nodes with a single neighbour.
+            print(f"--- TASK 13 ---")
+            time_start = time.time()
 
-    ########################################
-    ### Task 13: Count nodes with a single neighbour.
-    print(f"--- TASK 13 ---")
-    time_start = time.time()
+            single_neighbour_count = 0
+            all_nodes = db.query("SELECT * FROM Nodes PARALLEL;")
+            for i, node in enumerate(all_nodes):
+                neighbours = find_neighbours(node['id'])
+                if len(neighbours) == 1:
+                    single_neighbour_count += 1
 
-    single_neighbour_count = 0
-    all_nodes = db.query("SELECT * FROM Nodes PARALLEL;")
-    for i, node in enumerate(all_nodes):
-        neighbours = find_neighbours(node['id'])
-        if len(neighbours) == 1:
-            single_neighbour_count += 1
+            print(f"Total nodes with a single neighbour: {single_neighbour_count}")
 
-        if i % 10000 == 0 and i != 0:
-            print(f"Progress: {(i / len(all_nodes)) * 100:.2f}%")
-
-    print(f"Total nodes with a single neighbour: {single_neighbour_count}")
-
-    time_end = time.time()
-    print_time(time_start, time_end)
+            time_end = time.time()
+            print_time(time_start, time_end)
 
